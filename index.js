3ee60e93428d57c1a05aab675aebfd707e86b166
Adonias <helpdesk1@safetydocs.com.br>
2019-02-28 14:53:42 -0300

    adicionando comentario no arquivo

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 
-
+/*
+autor:Adonias
+data: 2019/02/28
+versão: 1.0
+revisão: 0 
+Parâmetros: mes e ano inicial e mes e ano final
+Retorno: array()
+ */
 var rangeMes = function(mesInicio, anoInicio, mesFim, anoFim){
-
+	// declaro variáveis 
 	resultado = [];
 	anosIntervalo = [];
 
+	// validação de datas
 	if (anoInicio > anoFim) {
 
 		return "Data invalida";
@@ -14,23 +22,25 @@ var rangeMes = function(mesInicio, anoInicio, mesFim, anoFim){
 		return "Data invalida";
 
 	} else {
-
+		//verifico se esta no mesmo período para montar a matriz
 		if (anoInicio == anoFim){
 			for(mesInicio; mesInicio <= mesFim; mesInicio++){
 				resultado.push({"Mes: " : mesInicio, "Ano: " : anoInicio});
 			}
-		} else if (anoFim > anoInicio) {
-
+		} 
+		// verifico se exite mais de um ano para montar a matriz
+		else if (anoFim > anoInicio) {
+			// valida a quantidade de meses dentro no ano
 			for(mesInicio; mesInicio <= 12; mesInicio++){
 				resultado.push({"Mes: " : mesInicio, "Ano: " : anoInicio});
 			}
-
+			// valida o final do período, para montar o intervalo de anos
 			if ((anoFim - anoInicio) > 1) {
 				for(i = anoInicio + 1; i <= (anoFim - 1); i++){
 					anosIntervalo.push(i);
 				}
 			}
-
+			
 			for(i = 0; i < anosIntervalo.length; i++){
 				for(mes = 1; mes <= 12; mes++){
 					resultado.push({"Mes: " : mes, "Ano: " : anosIntervalo[i]});
@@ -44,7 +54,7 @@ var rangeMes = function(mesInicio, anoInicio, mesFim, anoFim){
 		}
 
 		return resultado;
-		
+
 	}
 
 }
